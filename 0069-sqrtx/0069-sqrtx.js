0479d4c14ef5aeda597debb796fdde5def6e43c2
afeefa12 <85550119+afeefa12@users.noreply.github.com>
2025-05-13 16:27:28 +0530

    Time: 0 ms (100%), Space: 57.2 MB (20.38%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.js
@@ -0,0 +1,11 @@
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var mySqrt = function(x) {
+    let num =Math.sqrt(x)
+    let num1= Math.floor(num)
+    return num1;
+};
+
+console.log(mySqrt(4));
