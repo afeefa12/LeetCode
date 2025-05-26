740e829ef24c667f3b6ac40d62d9beb4765e0d1e
afeefa12 <85550119+afeefa12@users.noreply.github.com>
2025-05-26 09:42:43 +0530

    Time: 0 ms (100%), Space: 56.9 MB (23.18%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.js
+++ 0020-valid-parentheses/0020-valid-parentheses.js
@@ -17,4 +17,23 @@ function isValid(s) {
     }
 
     return stack.length === 0; 
+}/**
+ * @param {string} s
+ * @return {boolean}
+ */
+function isValid(s) {
+    const stack = [];
+    const bracketMap = { ')': '(', ']': '[', '}': '{' };
+
+    for (let char of s) {
+        if (char === '(' || char === '[' || char === '{') {
+            stack.push(char); 
+        } else {
+            if (stack.length === 0 || stack.pop() !== bracketMap[char]) {
+                return false; 
+            }
+        }
+    }
+
+    return stack.length === 0; 
 }
